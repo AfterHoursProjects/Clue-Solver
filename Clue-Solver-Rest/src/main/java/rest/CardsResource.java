42dde8463acb0984a1a43f8b987f1140f7c1d92e
Matt McClellan <mcclellan.mj@gmail.com>
2012-04-29 17:48:51 -0500

    Created cards service

--- Clue-Solver-Rest/src/main/java/rest/CardsResource.java
+++ Clue-Solver-Rest/src/main/java/rest/CardsResource.java
@@ -1,11 +1,24 @@
 package rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import model.Card;
+
 import org.restlet.resource.ServerResource;
 
 /**
  * @author matt
  * 
  */
+@Path("cards")
 public class CardsResource extends ServerResource {
 
+	@PUT
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
+	public void insertCard(final Card toInsert) {
+
+	}
 }
