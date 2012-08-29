d3b5c3098b957eff4ff765e5a895afd6ea835ec0
Carl Hostrander <chostrander@franklinamerican.com>
2012-08-29 16:31:56 -0500

    started to add the buttons to the eliminate card page - dynamically

--- Clue-Solver-JSF/src/main/java/com/hoss/jsf/MainAction.java
+++ Clue-Solver-JSF/src/main/java/com/hoss/jsf/MainAction.java
@@ -1,9 +1,14 @@
 package com.hoss.jsf;
 
-
+import enums.RoomEnum;
+import enums.SuspectEnum;
+import enums.WeaponEnum;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIColumn;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlPanelGrid;
 import model.Triple;
 import model.TripleList;
 import model.rest.ProbabilityReport;
@@ -16,62 +21,96 @@ import service.ServerService;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author chostrander
  */
-@ManagedBean(name="mainAction")
+@ManagedBean(name = "mainAction")
 @SessionScoped
 public class MainAction {
-    
-        private List<Triple> remainingTriples;
-        
-        private String mostLikelyTripleTxt;
-        
-    	public void updateMostLikelyTriple() {
-            final Reference reference = new Reference("http://localhost/clue/probability.json");
-            reference.setHostPort(ServerService.getPort());
 
-            final ClientResource resource = new ClientResource(reference);
-            resource.setProtocol(Protocol.HTTP);
-            resource.setChallengeResponse(ServerService.getChallengeResponse());
+    private HtmlPanelGrid eliminateCardComponenets = null;
+    private List<Triple> remainingTriples;
+    private String mostLikelyTripleTxt;
 
-            ProbabilityReport report = resource.get(ProbabilityReport.class);
-            Triple mostLikelyTriple = report.getMostLikelyTriple().getWrappedObject();
-            this.mostLikelyTripleTxt =  mostLikelyTriple.toString();
-            resource.release();
-	}
-        
-        public String getMostLikelyTripleTxt() {
-            updateMostLikelyTriple();
-            
-            return this.mostLikelyTripleTxt;
+    public void updateMostLikelyTriple() {
+        final Reference reference = new Reference("http://localhost/clue/probability.json");
+        reference.setHostPort(ServerService.getPort());
+
+        final ClientResource resource = new ClientResource(reference);
+        resource.setProtocol(Protocol.HTTP);
+        resource.setChallengeResponse(ServerService.getChallengeResponse());
+
+        ProbabilityReport report = resource.get(ProbabilityReport.class);
+        Triple mostLikelyTriple = report.getMostLikelyTriple().getWrappedObject();
+        this.mostLikelyTripleTxt = mostLikelyTriple.toString();
+        resource.release();
+    }
+
+    public HtmlPanelGrid getEliminateCardComponents() {
+        UIColumn col1 = new UIColumn();
+        UIColumn col2 = new UIColumn();
+        UIColumn col3 = new UIColumn();
+        HtmlCommandButton hcb;
+        for (String data : RoomEnum.getStringValues()) {
+            hcb = new HtmlCommandButton();
+            hcb.setValue(data);
+
+            col1.getChildren().add(hcb);
+        }
+
+        for (String data : SuspectEnum.getStringValues()) {
+            hcb = new HtmlCommandButton();
+            hcb.setValue(data);
+
+            col2.getChildren().add(hcb);
+        }
+
+        for (String data : WeaponEnum.getStringValues()) {
+            hcb = new HtmlCommandButton();
+            hcb.setValue(data);
+
+            col3.getChildren().add(hcb);
         }
         
-        public void setMostLikelyTripleTxt (String value) {
-            this.mostLikelyTripleTxt = value;
+        if (this.eliminateCardComponenets == null) {
+            this.eliminateCardComponenets = new HtmlPanelGrid();
         }
-        
+        this.eliminateCardComponenets.getChildren().add(col1);
+        this.eliminateCardComponenets.getChildren().add(col2);
+        this.eliminateCardComponenets.getChildren().add(col3);
 
-	public List<Triple> getRemainingTriples() {
-		final Reference reference = new Reference("http://localhost/clue/triples/remaining.json");
-		reference.setHostPort(ServerService.getPort());
+        return this.eliminateCardComponenets;
 
-		final ClientResource resource = new ClientResource(reference);
-		resource.setProtocol(Protocol.HTTP);
-		resource.setChallengeResponse(ServerService.getChallengeResponse());
+    }
 
-		final TripleList response = resource.get(TripleList.class);
-		resource.release();
+    public String getMostLikelyTripleTxt() {
+        updateMostLikelyTriple();
 
-		this.remainingTriples = response;
-                
-                return this.remainingTriples;
-	}
-        
-        public void setRemainingTriples(List<Triple> values) {
-            this.remainingTriples = values;
-        }
-    
+        return this.mostLikelyTripleTxt;
+    }
+
+    public void setMostLikelyTripleTxt(String value) {
+        this.mostLikelyTripleTxt = value;
+    }
+
+    public List<Triple> getRemainingTriples() {
+        final Reference reference = new Reference("http://localhost/clue/triples/remaining.json");
+        reference.setHostPort(ServerService.getPort());
+
+        final ClientResource resource = new ClientResource(reference);
+        resource.setProtocol(Protocol.HTTP);
+        resource.setChallengeResponse(ServerService.getChallengeResponse());
+
+        final TripleList response = resource.get(TripleList.class);
+        resource.release();
+
+        this.remainingTriples = response;
+
+        return this.remainingTriples;
+    }
+
+    public void setRemainingTriples(List<Triple> values) {
+        this.remainingTriples = values;
+    }
 }
